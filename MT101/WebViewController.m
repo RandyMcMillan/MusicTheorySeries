44fa474144445988d8b066694de3932225e589d6
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2012-08-29 06:22:45 -0400

    ..

--- MT101/WebViewController.m
+++ MT101/WebViewController.m
@@ -18,10 +18,10 @@
 
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-	if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-		// Initialization code
-	}
-	return self;
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Initialization code
+    }
+    return self;
 }
 
 /*
@@ -33,21 +33,21 @@
 /*
  If you need to do additional setup after loading the view, override viewDidLoad. */
 - (void)viewDidLoad {
-  
-    
+
+
     CGRect textFieldFrame = CGRectMake(kLeftMargin, kTweenMargin,
-									   self.view.bounds.size.width - (kLeftMargin * 2.0), kTextFieldHeight);
-	
+        self.view.bounds.size.width - (kLeftMargin * 2.0), kTextFieldHeight);
+
     addressLabel.frame = textFieldFrame;
-    
-    
+
+
 #if TARGET_IPHONE_SIMULATOR
   //  [NSClassFromString(@"WebView") _enableRemoteInspector];
     //NSLog(@"MediaBrowserViewController.m Line 76. This makes ChildBrowser debbugging in the desktop browser possible http://localhost:9999/?page=1 or 2 ");
 #endif
-    
-    
-    
+
+
+
     refreshBtn.image = [UIImage imageNamed:[[self class] resolveImageResource:@"WebView.bundle/but_refresh"]];
     backBtn.image = [UIImage imageNamed:[[self class] resolveImageResource:@"WebView.bundle/arrow_left"]];
     fwdBtn.image = [UIImage imageNamed:[[self class] resolveImageResource:@"WebView.bundle/arrow_right"]];
@@ -57,25 +57,25 @@
     fwdBtn.enabled = webView.canGoForward;
 
     webView.delegate = self;
-    
-    
-    
-    
+
+
+
+
 //	NSString *urlAddress = @"http://www.google.com";
-	
-	//Create a URL object.
-//	NSURL *url = [NSURL URLWithString:urlAddress];
-	
-	//URL Requst Object
-//	NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
-	
-	//Load the request in the UIWebView.
-//	[webView loadRequest:requestObj];
+
+    //Create a URL object.
+//SURL *url = [NSURL URLWithString:urlAddress];
+
+    //URL Requst Object
+//NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+
+    //Load the request in the UIWebView.
+    //[webView loadRequest:requestObj];
 }
 
 -(IBAction) onSafariButtonPress:(id)sender
 {
-    
+
  //   if(delegate != NULL)
    // {
      //   [delegate onOpenInSafari];
@@ -107,15 +107,15 @@
 }
 
 -(IBAction) onDoneButtonPress:(id)sender {
-    
+
     [ self closeBrowser];
 }
 
 
 -(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType; {
-    
+
     NSLog(@"shouldStartLoadWithRequest");
-    
+
     return YES;
 }
 
@@ -124,28 +124,23 @@
 {
     NSString* systemVersion = [[UIDevice currentDevice] systemVersion];
     BOOL isLessThaniOS4 = ([systemVersion compare:@"4.0" options:NSNumericSearch] == NSOrderedAscending);
-    
+
     if (isLessThaniOS4) {
-        
+
         return [NSString stringWithFormat:@"%@.png", resource];
-        
+
     } else {
-        
+
         if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] == YES && [[UIScreen mainScreen] scale] == 2.00) {
-            
+
             return [NSString stringWithFormat:@"%@@2x.png", resource];
-            
+
         }
-        
+
     }
-    
+
     return resource;//if all else fails
 }
-
-
-
-
-
 
 
 - (void)webViewDidStartLoad:(UIWebView *)sender {
@@ -156,11 +151,11 @@
     NSLog(@"webViewDidStartLoad");
     spinner.hidden = FALSE;
     [ spinner startAnimating ];
-    
+
 }
 
-- (void)webViewDidFinishLoad:(UIWebView *)sender
-{
+- (void)webViewDidFinishLoad:(UIWebView *)sender {
+
     NSURLRequest *request = webView.request;
     NSLog(@"New Address is : %@",request.URL.absoluteString);
     addressLabel.text = request.URL.absoluteString;
@@ -170,72 +165,67 @@
     [ spinner stopAnimating ];
     NSLog(@"webViewDidFinLoad");
  
-    if(delegate != NULL)
-    {
+    if(delegate != NULL) {
         //[delegate onChildLocationChange:request.URL.absoluteString];
     }
-    
+
     spinner.hidden = TRUE;
 
-    
+
 }
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
-{
-	// load error, hide the activity indicator in the status bar
-	//[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-    
-	// report the error inside the webview
-	NSString* errorString = [NSString stringWithFormat:
-							 @"<html><center><font size=+5 color='red'>An error occurred:<br>%@</font></center></html>",
-							 error.localizedDescription];
-	[self.webView loadHTMLString:errorString baseURL:nil];
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+
+    // load error, hide the activity indicator in the status bar
+    //[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+
+    // report the error inside the webview
+    NSString* errorString = [NSString stringWithFormat:
+                                 @"<html><center><font size=+5 color='red'>An error occurred:<br>%@</font></center></html>",
+                             error.localizedDescription];
+    [self.webView loadHTMLString:errorString baseURL:nil];
 }
-
-
-
 
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
-    
-    
+
+
     // Return YES for supported orientations
-    if (toInterfaceOrientation == UIInterfaceOrientationLandscapeRight){
+    if (toInterfaceOrientation == UIInterfaceOrientationLandscapeRight) {
         return YES;
     }
     
-    if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft){
+    if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
         return YES;
     }
-    
-    if (toInterfaceOrientation == UIInterfaceOrientationPortrait){
+
+    if (toInterfaceOrientation == UIInterfaceOrientationPortrait) {
         return YES;
     }
-    
-    if (toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown){
+
+    if (toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
         return YES;
     } else {return NO;}
-    
+
 }
 
 - (BOOL)didAutorotateToInterfaceOrientation:(UIInterfaceOrientation)currentInterfaceOrientation {
-    
+
     NSLog(@"did auto rotate");
     return YES;
-    
+
 }
 
 
-
 - (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
-	// Release anything that's not essential, such as cached data
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
 }
 
 
 - (void)dealloc {
-	[webView release];
-	[super dealloc];
+    [webView release];
+    [super dealloc];
 }
 
 
