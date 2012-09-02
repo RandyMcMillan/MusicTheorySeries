862c49a4160de4b8077286a0ea6fc58bec4688c0
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2012-09-02 17:49:43 -0400

    git reset --soft HEAD~1
    git reset --soft HEAD~1

--- MT101/GradientButton.h
+++ MT101/GradientButton.h
@@ -1,10 +1,4 @@
 //
-
-//  ButtonGradientView.h
-//  Custom Alert View
-//
-//  Created by jeff on 5/17/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- MT101/GradientButton.m
+++ MT101/GradientButton.m
@@ -1,10 +1,4 @@
 //
-
-//  ButtonGradientView.m
-//  Custom Alert View
-//
-//  Created by jeff on 5/17/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
 #import "GradientButton.h"
