2b517fabfb8dd8ea58d555cf35a450ae33f4ea4e
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2012-09-10 05:03:05 -0400

    testing Gcommit

--- MT101/MT101/ViewControllers/DetailViewController.m
+++ MT101/MT101/ViewControllers/DetailViewController.m
@@ -11,10 +11,10 @@
 
 
 @interface DetailViewController (){
-    
+
     MPMoviePlayerViewController *moviePlayer;
     AppleXylophoneViewController *test;
-    
+
 }
 @property (strong, nonatomic) UIPopoverController *masterPopoverController;
 - (void)configureView;
@@ -34,60 +34,60 @@
 
 
     NSLog(@"displayInteractive");
-    
+
     AppleXylophoneViewController *test = [[AppleXylophoneViewController alloc]init];
     //[self.view addSubview:test.view];
    // test.modalTransitionStyle = UIViewAnimationOptionTransitionFlipFromLeft;
    // test.modalPresentationStyle = UIViewAnimationOptionTransitionFlipFromLeft;
     //self.modalPresentationStyle = UIModalPresentationPageSheet;
     [self presentModalViewController:test animated:YES];
-    
+
 
 }
 
 -(IBAction)playMovie:(id)sender{
-    
+
    // NSBundle *bundle = [NSBundle mainBundle];
-	
-	NSURL *movieURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:MovieToPlay ofType:@"mp4"]];
-    
+
+    NSURL *movieURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:MovieToPlay ofType:@"mp4"]];
+
    moviePlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:movieURL];
-    
+
     //---play partial screen---
-    
+
     moviePlayer.view.frame =imageView.bounds; 
     moviePlayer.view.frame = imageView.frame;
     //[imageView addSubview:moviePlayer.view];
 
-    
+
 
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(moviePlayBackDidFinish:)
                                                  name:MPMoviePlayerPlaybackDidFinishNotification
                                                object:moviePlayer.moviePlayer];
 
-        
+
     [self.view addSubview:moviePlayer.view];
 
-    
-	//[self presentMoviePlayerViewControllerAnimated:moviePlayer];	
+
+    //[self presentMoviePlayerViewControllerAnimated:moviePlayer];
 }
 
-- (void) moviePlayBackDidFinish:(NSNotification*)notification {  
-    
+- (void) moviePlayBackDidFinish:(NSNotification*)notification {
+
     NSLog(@"In playback Did Finish");
-    
-    MPMoviePlayerController *moviePlayer = [notification object];  
-    [[NSNotificationCenter defaultCenter] removeObserver:self  
-                                                    name:MPMoviePlayerPlaybackDidFinishNotification  
-                                                  object:moviePlayer];  
-    
-    // If the moviePlayer.view was added to the view, it needs to be removed  
-    if ([moviePlayer respondsToSelector:@selector(setFullscreen:animated:)]) {  
-        [moviePlayer.view removeFromSuperview];  
-    }  
-    
-        
+
+    MPMoviePlayerController *moviePlayer = [notification object];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:MPMoviePlayerPlaybackDidFinishNotification
+                                                  object:moviePlayer];
+
+    // If the moviePlayer.view was added to the view, it needs to be removed
+    if ([moviePlayer respondsToSelector:@selector(setFullscreen:animated:)]) {
+        [moviePlayer.view removeFromSuperview];
+    }
+
+
 }
 
 
@@ -95,14 +95,14 @@
 {
     if (_detailItem != newDetailItem) {
         _detailItem = newDetailItem;
-        
+
         // Update the view.
         [self configureView];
     }
 
     if (self.masterPopoverController != nil) {
         [self.masterPopoverController dismissPopoverAnimated:YES];
-    }        
+    }
 }
 
 - (void)configureView
@@ -117,7 +117,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
     [self configureView];
    self.toolBar.alpha = 1.0;
 
@@ -137,19 +137,19 @@
     if (toInterfaceOrientation == UIInterfaceOrientationLandscapeRight){
         return YES;
     }
-    
+
     if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft){
         return YES;
     }
-    
+
     if (toInterfaceOrientation == UIInterfaceOrientationPortrait){
         return NO;
     }
-    
+
     if (toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown){
         return NO;
     } else {return NO;}
-    
+
 
 
 
@@ -160,11 +160,11 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         //self.title = NSLocalizedString(@"Video Player", @"Video Player");
-        
+
     }
     return self;
 }
-							
+
 #pragma mark - Split view
 
 - (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
