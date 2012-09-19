e387409180af145c526ba4b2db40724281d32f1d
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2012-09-19 18:56:01 -0400

    Revert "CALayer methods added to GrandstaffVC"
    This reverts commit bc363a18b5d0415946e3cecd3e19ab923058d941.

--- MT101/MT101/GrandStaffViewController.m
+++ MT101/MT101/GrandStaffViewController.m
@@ -5,9 +5,6 @@
 #import "GrandStaffViewController.h"
 #import "GradientButton.h"
 #import "GradientToolBar.h"
-#import <QuartzCore/QuartzCore.h>
-#import "Common.h"
-
 @implementation GrandStaffViewController
 
 @synthesize mixerHost;
@@ -480,47 +477,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-   
-    
-    
-    CALayer *sublayer = [CALayer layer];
-    sublayer.backgroundColor = [UIColor blueColor].CGColor;
-    sublayer.shadowOffset = CGSizeMake(0, 3);
-    sublayer.shadowRadius = 5.0;
-    sublayer.shadowColor = [UIColor blackColor].CGColor;
-    sublayer.shadowOpacity = 0.8;
-    sublayer.frame = CGRectMake(30, 30, 128, 192);
-    sublayer.borderColor = [UIColor blackColor].CGColor;
-    sublayer.borderWidth = 2.0;
-    sublayer.cornerRadius = 10.0;
-    [self.view.layer addSublayer:sublayer];
-
-    
-    CALayer *customDrawn = [CALayer layer];
-    customDrawn.delegate = self;
-    customDrawn.backgroundColor = [UIColor greenColor].CGColor;
-    customDrawn.frame = CGRectMake(30, 250, 128, 40);
-    customDrawn.shadowOffset = CGSizeMake(0, 3);
-    customDrawn.shadowRadius = 5.0;
-    customDrawn.shadowColor = [UIColor blackColor].CGColor;
-    customDrawn.shadowOpacity = 0.8;
-    customDrawn.cornerRadius = 10.0;
-    customDrawn.borderColor = [UIColor blackColor].CGColor;
-    customDrawn.borderWidth = 2.0;
-    customDrawn.masksToBounds = YES;
-    [self.view.layer addSublayer:customDrawn];
-    [customDrawn setNeedsDisplay];
-
-    
-    
     [closeButton useDoneButtonStyle];
     [mixerHost stopAUGraph];
 
     UIImageView *exampleImageView = [[UIImageView alloc] initWithImage:[UIImage originalSizeImageWithPDFNamed:@"TheGrandStaff.pdf"]];
-        imageView.image = exampleImageView.image;
+    imageView.image = exampleImageView.image;
     [exampleImageView release];
 
-    // [imageView useGrandStaffStyle];
+    [imageView useGrandStaffStyle];
 
     [[UIDevice currentDevice]
         beginGeneratingDeviceOrientationNotifications];
@@ -534,54 +498,6 @@
     // start the audio graph
     [mixerHost startAUGraph];
 } /* viewDidLoad */
-
-
-void MyDrawColoredPattern (void *info, CGContextRef context) {
-    
-    CGColorRef dotColor = [UIColor colorWithHue:0 saturation:0 brightness:0.07 alpha:1.0].CGColor;
-    CGColorRef shadowColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.1].CGColor;
-    
-    CGContextSetFillColorWithColor(context, dotColor);
-    CGContextSetShadowWithColor(context, CGSizeMake(0, 1), 1, shadowColor);
-    
-    CGContextAddArc(context, 3, 3, 4, 0, radians(360), 0);
-    CGContextFillPath(context);
-    
-    CGContextAddArc(context, 16, 16, 4, 0, radians(360), 0);
-    CGContextFillPath(context);
-    
-}
-
-- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)context {
-    
-    CGColorRef bgColor = [UIColor colorWithHue:0.6 saturation:1.0 brightness:1.0 alpha:1.0].CGColor;
-    CGContextSetFillColorWithColor(context, bgColor);
-    CGContextFillRect(context, layer.bounds);
-    
-    static const CGPatternCallbacks callbacks = { 0, &MyDrawColoredPattern, NULL };
-    
-    CGContextSaveGState(context);
-    CGColorSpaceRef patternSpace = CGColorSpaceCreatePattern(NULL);
-    CGContextSetFillColorSpace(context, patternSpace);
-    CGColorSpaceRelease(patternSpace);
-    
-    CGPatternRef pattern = CGPatternCreate(NULL,
-                                           layer.bounds,
-                                           CGAffineTransformIdentity,
-                                           24,
-                                           24,
-                                           kCGPatternTilingConstantSpacing,
-                                           true,
-                                           &callbacks);
-    CGFloat alpha = 1.0;
-    CGContextSetFillPattern(context, pattern, &alpha);
-    CGPatternRelease(pattern);
-    CGContextFillRect(context, layer.bounds);
-    CGContextRestoreGState(context);
-}
-
-
-
 
 - (void)orientationChanged:(NSNotification *)notification
 {
