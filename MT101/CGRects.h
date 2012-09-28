aedafd0ece1a82b52fb3467218aa173eec72687a
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2012-09-28 05:15:12 -0400

    CGRects

---
+++ MT101/CGRects.h
@@ -0,0 +1,20 @@
+//
+//  CGRects.h
+//  MT101
+//
+//  Created by Randy McMillan on 9/28/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CGRects : NSObject
+{
+    CGRect *C2;
+}
+
+@property (nonatomic) CGRect           *C2;
+
+- (void)useC2;//:(id)sender;
+
+@end
